a33d8017308e50ea7026ea8b972a7bc112ef8a8e
haoxle <hao-le@live.co.uk>
2022-11-23 13:30:29 +0000

    paragraph updated

--- src/pages/Bio/Bio.jsx
+++ src/pages/Bio/Bio.jsx
@@ -6,14 +6,22 @@ const Bio = () => {
       <div className="profile">
         <img className="profile_picture" src={profile} alt="me" />
         <p className="profile_description">
-          I was previously working as a Clinical Pharmacist where I was able to
-          develop many transferable skills. I discovered tech by chance through
-          a business venture where I had to support a software engineer on
-          creating my website. The process was so intriguing and it encouraged
-          me to look into coding via codeAcademy and freecodecamp. What excited
-          me most was continuously facing new challenges and finding smart ways
-          to solve problems. I pursued further by joining _nology to improve on
-          my knowledge and understanding and move industries.
+          I was previously working as a Clinical Pharmacist in General Practice.
+          My duties involved supporting patients with medicines management and
+          focused on long-term prescribing in specialised areas such as:
+          Diabetes, Hypertension, and many more. In addition to my expanding
+          knowledge in treating co-morbidities, I was able to develop
+          fundamental transferable soft skills, such as communication,
+          leadership, and most importantly accountability. I discovered tech by
+          chance, through a business venture where I had to support a software
+          engineer in creating my website. The process was intriguing and it
+          encouraged me to look into coding via Codecademy and freecodecamp.
+          What excited me most, was continuously facing new challenges and
+          finding smart ways to solve problems. At the time I was looking to
+          change careers, and the decision to pursue developing was
+          straightforward. Shortly after, I was recruited by _nology. Through
+          _nology robust training program, I was able to produce various
+          projects which can be seen on my projects page.
         </p>
       </div>
     </>
