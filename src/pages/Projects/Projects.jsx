228429b2d924ac0d86a49fdea1af7c5275df23ec
Hao <hao-le@live.co.uk>
2022-11-24 15:03:20 +0000

    completed about  me page and added logos

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,14 +1,61 @@
 import "./Projects.scss";
-import embroidery from "../../assets/embroidery.jpg";
+import css from "../../assets/logos/css.png";
+import figma from "../../assets/logos/figma.png";
+import github from "../../assets/logos/github.png";
+import html from "../../assets/logos/html.png";
+import java from "../../assets/logos/java.png";
+import javascript from "../../assets/logos/javascript.png";
+import react from "../../assets/logos/react.png";
+import springboot from "../../assets/logos/springboot.png";
 
 const Projects = () => {
   return (
     <>
-      <div class="container">
-        <img src={embroidery} alt="Avatar" class="image" />
-        <div class="middle">
-          <div class="text">John Doe</div>
-        </div>
+      <h1>My Projects</h1>
+      <div>
+        <img src="d" alt="d" />
+        <h1>Project</h1>
+        <p>Information</p>
+        <img className="skills-image" src={css} alt="css" />
+        <img className="skills-image" src={figma} alt="figma" />
+        <img className="skills-image" src={github} alt="github" />
+        <img className="skills-image" src={html} alt="html" />
+        <img className="skills-image" src={java} alt="java" />
+        <img className="skills-image" src={javascript} alt="javascript" />
+        <img className="skills-image" src={react} alt="react" />
+        <img className="skills-image" src={springboot} alt="springboot" />
+
+        <a href="d">See More!</a>
+      </div>
+      <div>
+        <img src="d" alt="d" />
+        <h1>Project</h1>
+        <p>Information</p>
+        <a href="d">See More!</a>
+      </div>
+      <div>
+        <img src="d" alt="d" />
+        <h1>Project</h1>
+        <p>Information</p>
+        <a href="d">See More!</a>
+      </div>
+      <div>
+        <img src="d" alt="d" />
+        <h1>Project</h1>
+        <p>Information</p>
+        <a href="d">See More!</a>
+      </div>
+      <div>
+        <img src="d" alt="d" />
+        <h1>Project</h1>
+        <p>Information</p>
+        <a href="d">See More!</a>
+      </div>
+      <div>
+        <img src="d" alt="d" />
+        <h1>Project</h1>
+        <p>Information</p>
+        <a href="d">See More!</a>
       </div>
     </>
   );
