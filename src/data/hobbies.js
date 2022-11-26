40e51b72eb0f6f6f21af15a06c3c6f60a04c1e7c
Hao <hao-le@live.co.uk>
2022-11-26 23:17:16 +0000

    bio simplified further

--- src/data/hobbies.js
+++ src/data/hobbies.js
@@ -38,3 +38,22 @@ export const hobbies = [
       reaction from your friends and family.`,
   },
 ];
+
+export const aboutMe = [
+  `I was previously working as a Clinical Pharmacist in General Practice.
+  My duties involved supporting patients with medicines management and
+  focused on long-term prescribing in specialised areas such as:
+  Diabetes, Hypertension, and many more. In addition to my expanding
+  knowledge in treating co-morbidities, I was able to develop
+  fundamental transferable soft skills, such as communication,
+  leadership, and most importantly accountability. I discovered tech by
+  chance, through a business venture where I had to support a software
+  engineer in creating my website. The process was intriguing and it
+  encouraged me to look into coding via Codecademy and freecodecamp.
+  What excited me most, was continuously facing new challenges and
+  finding smart ways to solve problems. At the time I was looking to
+  change careers, and the decision to pursue developing was
+  straightforward. Shortly after, I was recruited by _nology. Through
+  _nology's robust training program, I was able to produce various
+  projects which can be seen on my projects page.`,
+];
--- src/pages/Bio/Bio.jsx
+++ src/pages/Bio/Bio.jsx
@@ -1,6 +1,6 @@
 import "./Bio.scss";
 import profile from "../../assets/profile.jpg";
-import { hobbies } from "../../data/hobbies";
+import { aboutMe, hobbies } from "../../data/hobbies";
 
 const Bio = () => {
   const hobby = hobbies.map((activity, i) => {
@@ -19,24 +19,7 @@ const Bio = () => {
     <>
       <div className="profile">
         <img className="profile_picture" src={profile} alt="me" />
-        <p className="profile_description">
-          I was previously working as a Clinical Pharmacist in General Practice.
-          My duties involved supporting patients with medicines management and
-          focused on long-term prescribing in specialised areas such as:
-          Diabetes, Hypertension, and many more. In addition to my expanding
-          knowledge in treating co-morbidities, I was able to develop
-          fundamental transferable soft skills, such as communication,
-          leadership, and most importantly accountability. I discovered tech by
-          chance, through a business venture where I had to support a software
-          engineer in creating my website. The process was intriguing and it
-          encouraged me to look into coding via Codecademy and freecodecamp.
-          What excited me most, was continuously facing new challenges and
-          finding smart ways to solve problems. At the time I was looking to
-          change careers, and the decision to pursue developing was
-          straightforward. Shortly after, I was recruited by _nology. Through
-          _nology's robust training program, I was able to produce various
-          projects which can be seen on my projects page.
-        </p>
+        <p className="profile_description">{aboutMe[0]}</p>
       </div>
       <div className="extracuricular">
         <h1 className="extracuricular__title">Hobbies</h1>
