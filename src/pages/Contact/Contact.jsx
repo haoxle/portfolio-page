2f890392bbc87365f09a814e51c1923888ceb259
Hao <hao-le@live.co.uk>
2022-11-26 12:05:22 +0000

    contact section done

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,7 +3,40 @@ import "./Contact.scss";
 const Contact = () => {
   return (
     <>
-      <h1>Contact</h1>
+      <h1 className="contact-greeting">Hello.</h1>
+      <p className="contact-message">Get in touch! </p>
+      <p className="contact-message">
+        Email: <span className="span">Hao-Le@live.co.uk</span>
+      </p>
+      <div className="contact">
+        <p>Connect with me!</p>{" "}
+        <div className="contact__message">
+          <a
+            href="https://www.instagram.com/haoxle/"
+            target="_blank"
+            rel="noreferrer"
+            className="contact__message"
+          >
+            <span className="span">Instagram</span>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/hao-le-376669107/"
+            target="_blank"
+            rel="noreferrer"
+            className="contact__message"
+          >
+            <span className="span">LinkedIn</span>
+          </a>
+          <a
+            href="https://github.com/haoxle"
+            target="_blank"
+            rel="noreferrer"
+            className="contact__message"
+          >
+            <span className="span">GitHub</span>
+          </a>
+        </div>
+      </div>
     </>
   );
 };
