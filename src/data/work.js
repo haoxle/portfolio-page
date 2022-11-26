5978439f14b3a2b9cbb4a053b0388e53dbb86887
Hao <hao-le@live.co.uk>
2022-11-26 16:00:21 +0000

    experience outline

---
+++ src/data/work.js
@@ -0,0 +1,32 @@
+export const work = [
+  {
+    title: "Clinical Pharmacist",
+    work: "Improving Health Limited",
+    date: "Mar 22 - Aug 22",
+    description: "",
+  },
+  {
+    title: "Clinical Pharmacist",
+    work: "South Down Health & Care",
+    date: "Aug 21 - Feb 22",
+    description: "",
+  },
+  {
+    title: "Trainee Pharmacist",
+    work: "Eastbourse Hospital",
+    date: "Jul 20 - Jul 21",
+    description: "",
+  },
+  {
+    title: "Nails & Spa",
+    work: "Assistant Manager",
+    date: "Jul 19 - Aug 22",
+    description: "",
+  },
+  {
+    title: "Magnet",
+    work: "Design Sales Assistant",
+    date: "Jan 20 - Jun 20",
+    description: "",
+  },
+];
--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -1,10 +1,16 @@
+import { work } from "../../data/work";
 import "./Experience.scss";
 const Experience = () => {
-  return (
-    <>
-      <h1>Work Experience</h1>
-    </>
-  );
+  const workExperience = work.map((job, i) => {
+    return (
+      <div className="work">
+        <h1 className="work__title">{job.title}</h1>
+        <h2 className="work__place">{job.work}</h2>
+        <p className="work__date">{job.date} </p>
+      </div>
+    );
+  });
+  return <div className="experience">{workExperience}</div>;
 };
 
 export default Experience;
