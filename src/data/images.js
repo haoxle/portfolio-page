f462cf55833e85d8f470295487633c89d009506f
Hao <hao-le@live.co.uk>
2022-11-26 10:56:46 +0000

    added skills

---
+++ src/data/images.js
@@ -0,0 +1,102 @@
+export const images = [
+  {
+    id: 1,
+    name: "Figma",
+    src: require("../assets/logos/figma.png"),
+  },
+  {
+    id: 2,
+    name: "CSS",
+    src: require("../assets/logos/css.png"),
+  },
+  {
+    id: 3,
+    name: "GitHub",
+    src: require("../assets/logos/github.png"),
+  },
+  {
+    id: 4,
+    name: "HTML",
+    src: require("../assets/logos/html.png"),
+  },
+  {
+    id: 5,
+    name: "Java",
+    src: require("../assets/logos/java.png"),
+  },
+  {
+    id: 6,
+    name: "Firebase",
+    src: require("../assets/logos/firebase.png"),
+  },
+  {
+    id: 7,
+    name: "JavaScript",
+    src: require("../assets/logos/javascript.png"),
+  },
+  {
+    id: 8,
+    name: "React",
+    src: require("../assets/logos/react.png"),
+  },
+  {
+    id: 9,
+    name: "Sass",
+    src: require("../assets/logos/sass.png"),
+  },
+  {
+    id: 10,
+    name: "Jest",
+    src: require("../assets/logos/jest.png"),
+  },
+  {
+    id: 11,
+    name: "SpringBoot",
+    src: require("../assets/logos/springboot.png"),
+  },
+  {
+    id: 12,
+    name: "inDesign",
+    src: require("../assets/logos/indesign.png"),
+  },
+  {
+    id: 13,
+    name: "Illustrator",
+    src: require("../assets/logos/illustrator.png"),
+  },
+  {
+    id: 14,
+    name: "Powerpoint",
+    src: require("../assets/logos/powerpoint.png"),
+  },
+  {
+    id: 15,
+    name: "Excel",
+    src: require("../assets/logos/excel.png"),
+  },
+  {
+    id: 16,
+    name: "Word",
+    src: require("../assets/logos/word.png"),
+  },
+  {
+    id: 17,
+    name: "Leadership",
+    src: require("../assets/logos/leadership.png"),
+  },
+  {
+    id: 18,
+    name: "Communication",
+    src: require("../assets/logos/talk.png"),
+  },
+  {
+    id: 19,
+    name: "Team Up Mentor",
+    src: require("../assets/logos/mentor.png"),
+  },
+  {
+    id: 20,
+    name: "Teamwork",
+    src: require("../assets/logos/team.png"),
+  },
+];
--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,10 +1,21 @@
 import "./Skills.scss";
+import { images } from "../../data/images.js";
+
 const Skills = () => {
-  return (
-    <>
-      <h1>Skills</h1>
-    </>
-  );
+  const photoDisplay = images.map((photo, i) => {
+    return (
+      <div className="skills-section">
+        <img
+          className="skills-section__image"
+          key={i}
+          src={photo.src}
+          alt={photo.name}
+        />
+        <h1 className="skills-section__name">{photo.name}</h1>
+      </div>
+    );
+  });
+  return <div className="skills-container">{photoDisplay}</div>;
 };
 
 export default Skills;
