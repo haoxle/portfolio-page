8f287de8a30192f41bbe875f7cb4e1215d868797
Hao <hao-le@live.co.uk>
2022-11-26 14:51:39 +0000

    home updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,22 @@
 import "./Home.scss";
-import homecandle from "../../assets/homecandle.jpg";
+import me from "../../assets/me.jpg";
 const Home = () => {
   return (
     <>
       <div className="home-container">
-        <img className="home-candle" src={homecandle} alt="candle" />
-      </div>{" "}
+        <div className="home-contents">
+          <h1 className="home-contents__title">Hi,</h1>
+          <h1 className="home-contents__title">I am Hao,</h1>
+          <h1 className="home-contents__title">a software engineer.</h1>
+          <h2 className="home-contents__jobs">
+            Potter, Candle Maker, Pharmacist and Mentor
+          </h2>
+          <p className="home-contents__slogan">
+            I design, melt wax and throw clay
+          </p>
+        </div>
+        <img className="home-pic" src={me} alt="candle" />
+      </div>
     </>
   );
 };
